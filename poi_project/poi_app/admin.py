7b12d037584bd99ba1ecb1e3f8e1328e9fffe85e
Ahmed Kino <ehmedaff@gmail.com>
2024-02-24 23:16:50 +0000

    update admin panel view

--- poi_project/poi_app/admin.py
+++ poi_project/poi_app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import PointOfInterest
 
-admin.site.register(PointOfInterest)
+class PointOfInterestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'poi_name', 'poi_external_id', 'poi_category', 'avg_rating')
+    list_filter = ('poi_category',)
+    search_fields = ('poi_name', 'poi_external_id')
+
+admin.site.register(PointOfInterest, PointOfInterestAdmin)
